cc45df10c9f0e5f2ca76d9f086363163face60da
Your Name <you@example.com>
2026-01-06 13:19:12 -0300

    cambio en vite.config despliegue vercel

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/SOAB/", // 👈 AGREGÁ ESTA LÍNEA (con las barras al principio y al final)
+  //base: "/SOAB/",  👈 AGREGÁ ESTA LÍNEA (con las barras al principio y al final) esto es pata git pages
+  base: "/" // esto es para vercel
 })
